ad84be242176ab3bec019525752d3a5db676bcfd
VitoMars <vito95v@gmail.com>
2023-03-16 12:13:47 +0100

    Eliminato useRef

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
 import ControlledInput from "./components/ControlledInput";
-import RefExample from "./components/RefExample";
 
 function App() {
   return (
     <div className="App">
-      <hr />
       <ControlledInput />
-      <hr />
-      <RefExample />
     </div>
   );
 }
